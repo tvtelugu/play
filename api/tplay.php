75f7b9a022b5fe6b9b8224c0a742ba3d58aeaa6c
TVTelugu <63017421+tvtelugu@users.noreply.github.com>
2024-07-21 23:41:43 +0530

    Update tplay.php

--- api/tplay.php
+++ api/tplay.php
@@ -67,6 +67,8 @@
                         key: videoConfig.key
                     }
                 },
+                width: "100%",
+                height: "100%",
                 autostart: true,
                 mute: false,
                 primary: "html5",
